82e0587868a49c3642850698dc7cef9bd767c4fc
SilasReinagel <silas.reinagel@gmail.com>
2020-04-08 22:21:35 -0700

    Added Video Logo Scene. Closes #9

---
+++ src/EnigmaDragonsProject/Assets/Core/Content/VideoLogoScene/NavigateToNextIndexedScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public sealed class NavigateToNextIndexedScene : MonoBehaviour
+{
+    public void Go()
+    {
+        if (SceneManager.sceneCount > 1)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
