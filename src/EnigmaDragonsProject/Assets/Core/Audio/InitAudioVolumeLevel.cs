fe2860d73fdd39f5f037177622cc865d2c55976b
SilasReinagel <silas.reinagel@gmail.com>
2020-07-13 18:35:04 -0700

    Updated with Latest from The Interview and Infiltrator GO

--- src/EnigmaDragonsProject/Assets/Core/Audio/InitAudioVolumeLevel.cs
+++ src/EnigmaDragonsProject/Assets/Core/Audio/InitAudioVolumeLevel.cs
@@ -13,7 +13,9 @@ public sealed class InitAudioVolumeLevel : MonoBehaviour
     private void Start()
     {
         var volume = PlayerPrefs.GetFloat(valueName, 0.5f);
-        mixer.SetFloat(valueName, (Mathf.Log10(volume) * 20) - reductionDb);
+        var mixerVolume = (Mathf.Log10(volume) * 20) - reductionDb;
+        //Debug.Log($"Init - Set Audio Level for {valueName} to {volume} ({mixerVolume}db)", gameObject);
+        mixer.SetFloat(valueName, mixerVolume);
         if (player != null && demoSound != null)
             player.PlayOneShot(demoSound, 1f);
     }
